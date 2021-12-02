eb4e66c49affc27ebbd58f51a3ba9d37efcccefd
Francis Simy Nazareth <simy@outlook.com>
2021-12-02 20:50:15 +0300

    Health Stats

---
+++ HealthStats/Controllers/HealthStatsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HealthStats.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthStatsController : ControllerBase
+    {
+        // GET: api/<HealthStatsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<HealthStatsController>/5
+        [HttpGet("{id}")]
+        public HealthStats Get(int id)
+        {
+
+            //int stepsWalked, int kilometersWalked, int caloriesBurned,
+            //int maxHeartRate, int minHeartRate, int userRanking
+            if (id == 1234)
+                {
+                    return new HealthStats(6700, 3.2f, 240, 132, 68, 45);
+                }
+                else
+                {
+                    return new HealthStats(12400, 6.4f, 490, 140, 72, 8);
+                }
+        }
+
+        // POST api/<HealthStatsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<HealthStatsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<HealthStatsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ HealthStats/HealthStats.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthStats
+{
+    public class HealthStats
+    {
+
+        public int stepsWalked { get; set; }
+
+        public float kilometersWalked { get; set; }
+
+        public int caloriesBurned { get; set; }
+
+        public int maxHeartRate  { get; set; }
+
+        public int minHeartRate { get; set; }
+
+        public int userRanking { get; set;  }
+
+        public HealthStats(int stepsWalked, float kilometersWalked, int caloriesBurned, int maxHeartRate, int minHeartRate, int userRanking)
+        {
+            this.stepsWalked = stepsWalked;
+            this.kilometersWalked = kilometersWalked;
+            this.caloriesBurned = caloriesBurned;
+            this.maxHeartRate = maxHeartRate;
+            this.minHeartRate = minHeartRate;
+            this.userRanking = userRanking;
+        }
+    }
+}
